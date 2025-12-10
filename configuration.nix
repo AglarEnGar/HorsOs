af681a96dab28c532296f01531ba450e94dc0e1f
AglarEnGar <luca.j.morgan@gmail.com>
2025-12-10 11:51:40 -0800

    server stuff

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     ./zsh.nix
     ./swapStuff.nix
+    ./sshServer.nix
   ];
   nix.settings.warn-dirty = false;
 
@@ -84,29 +85,6 @@
     #   "1.0.0.1"
     # ];
     # dnsovertls = "true";
-  };
-
-  programs.ssh = {
-    startAgent = true;
-  };
-  services.openssh = {
-    enable = true;
-    ports = [10001];
-    settings = {
-      PasswordAuthentication = true;
-      KbdInteractiveAuthentication = true;
-      PermitRootLogin = "no";
-			AllowUsers = [
-				"guest"
-			];
-    };
-
-  };
-
-	services.endlessh = {
-    enable = true;
-    port = 22;
-    openFirewall = true;
   };
 
   # Set your time zone.
@@ -265,69 +243,6 @@
     ];
   };
 
-# Create the guest user with restricted access
-  users.users.guest = {
-    isNormalUser = true;
-		hashedPassword = "$y$j9T$POzFaPSuGzXGzrc0p9upq.$e32LRoe9zd7xpJgRhzW4skbJuzrNcwRTTqqZ/TqmZr/";
-    home = "/home/guest";
-		createHome = true;
-		group = "repository";
-    shell = pkgs.bashInteractive;
-  };
-
-	users.groups.repository = {};
-
-  # 1. SSH Config
-  services.openssh.extraConfig = ''
-    Match User guest
-      ChrootDirectory /var/jail
-  '';
-
-	fileSystems."/var/jail/run/current-system/sw/bin" = {
-    device = "/run/current-system/sw/bin";
-    options = [ "bind" "ro" ];
-  };
-
-  # 2. Set up the Jail Filesystem
-  # We use fileSystems to bind mount the Nix store and essential paths
-  fileSystems."/var/jail/nix/store" = {
-    device = "/nix/store";
-    options = [ "bind" "ro" ];
-  };
-
-  fileSystems."/var/jail/bin" = {
-    device = "/run/current-system/sw/bin";
-    options = [ "bind" "ro" ];
-  };
-
-	fileSystems."/var/jail/proc" = {
-		device = "proc";
-		fsType = "proc";
-		options = [ "nosuid" "noexec" "nodev" ];
-	};
-
-  # 3. Create necessary directories and symlinks
-  systemd.tmpfiles.rules = [
-    # The jail root must be root:root 755
-    "d /var/jail 0755 root root - -"
-		"d /var/jail/run/current-system/sw/bin 0755 root root - -"
-		"d /var/jail/proc 0555 root root - -"
-    "d /var/jail/nix/store 0755 root root - -"
-    "d /var/jail/bin 0755 root root - -"
-		"d /var/jail/tmp 1777 root root - -"
-
-		# make the home
-    "d /var/jail/home/guest 0700 guest repository - -"
-    
-    # Optional: If you need /dev (often needed for interactive shells)
-    "d /var/jail/dev 0755 root root - -"
-		"c /var/jail/dev/null    0666 root root - 1:3"
-		"c /var/jail/dev/zero    0666 root root - 1:5"
-		"c /var/jail/dev/tty     0666 root root - 5:0"
-		"c /var/jail/dev/urandom 0666 root root - 1:9"
-		"c /var/jail/dev/random  0666 root root - 1:8"
-  ];
-
   # Printer setup and cofig
   # services.printing.enable = true;
   # services.printing.drivers = [
@@ -360,11 +275,10 @@
   };
   services.flatpak.enable = true;
 
-
-
   # Find my packages
   environment.systemPackages = with pkgs; [
-		postgresql
+    ddclient
+    postgresql
     dropbox
     p7zip
     opensnitch-ui
@@ -468,7 +382,7 @@
     cmake
     xorg.xmodmap
     inxi
-		psmisc
+    psmisc
   ];
 
   services.udev = {
